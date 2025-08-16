a47370fb702e641c9d0996d09e5b2333c6268a61
ATIS <john@4bitcrew.com>
2025-08-16 18:09:24 -0500

    Updated minimum run-time for package to support concurrency.

---
+++ Zip/Zip+Concurrency.swift
@@ -0,0 +1,9 @@
+// ********************** Zip+Concurrency **********************
+// * Copyright © 2025 Roy Marmelstein. All rights reserved.
+// * Created on 8/16/25, for Zip
+// * John Ayers <john@4bitcrew.com>
+// * Unauthorized copying of this file is strictly prohibited
+// ********************** Zip+Concurrency **********************
+
+
+import Foundation
