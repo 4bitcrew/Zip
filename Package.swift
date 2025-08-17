59cd5844becfc984b928785e318580674c8ed57b
ATIS <john@4bitcrew.com>
2025-08-16 19:50:04 -0500

    Fixed out of date OS versions

--- Package.swift
+++ Package.swift
@@ -4,6 +4,12 @@ import PackageDescription
 
 let package = Package(
     name: "Zip",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12),
+        .tvOS(.v15),
+        .watchOS(.v8)
+    ],
     products: [
         .library(name: "Zip", targets: ["Zip"])
     ],
